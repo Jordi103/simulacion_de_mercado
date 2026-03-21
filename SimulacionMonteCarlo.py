121720f69b26e67bb3afdb47ce029b5c0ee1a413
Jordi Pomada Foix <jordi.pomada@gmail.com>
2026-03-21 19:01:20 +0100

    Merton Jump Diffusion: ajuste de parámetros fallido.

--- SimulacionMonteCarlo.py
+++ SimulacionMonteCarlo.py
@@ -18,6 +18,9 @@ class SimulacionMonteCarlo(ABC):
         """
         pass
 
+    def dibuja_y_simula(self):
+        return self.simular()
+
 
 class GBM(SimulacionMonteCarlo):
 
@@ -35,11 +38,12 @@ class GBM(SimulacionMonteCarlo):
 
 
     def confirmar_parametros_validos(self, param_dict):
+        todo_bien = True
         nones = sum([var not in param_dict.keys() for var in ('Deltat', 'N', 'T')])
         if  nones >= 2:
             print("ERROR: Hay que informar como mínimo dos de los tres siguientes parámetros:")
             print("Deltat, T, N")
-            return False
+            todo_bien = False
         elif nones == 1:
             if 'Deltat' not in param_dict.keys():
                 param_dict['Deltat'] = param_dict['T'] / param_dict['N']
@@ -51,19 +55,22 @@ class GBM(SimulacionMonteCarlo):
             if param_dict['T']/param_dict['N'] != param_dict['Deltat']:
                 print("ERRROR: Los valores de los siguientes parámetros no son válidos:")
                 print("Deltat, T, N")
-                return False
+                todo_bien = False
         for var in ['mu', 'sigma', 'S0', 'M']:
             if var not in param_dict.keys():
                 print(f"ERROR: {var} debe estar informada.")
-                return False
+                todo_bien = False
             elif var != 'mu' and param_dict[var] < 0:
                 print(f"ERROR: {var} debe ser mayor o igual que cero.")
-                return False
+                todo_bien = False
         if param_dict['M'] == 0:
             print("WARNING: el número de simulaciones es cero.")
-            return False
+            todo_bien = False
+        if param_dict['S0'] <= 0:
+            print("ERROR: el precio inicial debe ser positivo.")
+            todo_bien = False
 
-        return True
+        return todo_bien
 
     
     def informar_parametros(self, param_dict):        
@@ -76,34 +83,54 @@ class GBM(SimulacionMonteCarlo):
             self.N = param_dict['N']
             self.M = param_dict['M']
             self.param_dict = param_dict
+        else:
+            print("ERROR: No se han asignado los parámetros.")
         
 
-    def simular(self, with_jumps=False):
-        if not self.confirmar_parametros_validos(self.param_dict):
+    def simular(self, S0=None, N=None, M=None):
+        new_param_dict = self.param_dict.copy()
+        if S0 is not None:
+            new_param_dict['S0'] = S0
+        if N is not None:
+            new_param_dict['N'] = N
+            new_param_dict['T'] = N * new_param_dict['Deltat']
+        if M is not None:
+            new_param_dict['M'] = M
+        
+        if not self.confirmar_parametros_validos(new_param_dict):
             print("ERROR: parámetros inválidos. Abortando simulación.")
             return
-
+        else:
+            self.informar_parametros(new_param_dict)
+        
         S = np.full([self.N+1, self.M], self.S0, dtype=np.dtype(float))
         Z = np.random.standard_normal([self.N, self.M])    
         
         for i in range(self.N):
             S[i+1,:] = S[i,:]*(1 + self.mu * self.Deltat + self.sigma * np.sqrt(self.Deltat)*Z[i, :])
+
         return S
         
 
     def mostrar_parametros(self):
-        # formatear esto debidamente
-        print(self.param_dict)
+        for var in self.param_dict.keys():
+            k = len(var)//8 + 2
+            if var == 'sigma_J':
+                k -= 1
+            espacio = k*"\t"
+            print(f"{var}:{espacio}{self.param_dict[var]}")
+        
+        
 
 
     def ajustar_parametros(self, ts, M = 1):
         # ajusta todos los parámetros o ninguno
-        new_params = {'T': ts.shape[0], 'N': ts.shape[0], 'Deltat':1., 'S0':ts[0], 'M':M}
+        new_params = {'T': ts.shape[0], 'N': ts.shape[0], 'Deltat':1., 'S0':ts.iloc[0], 'M':M}
         def neg_log_likelihood(p):
             var_mu, var_sigma = p
             nlL = -np.sum([np.log(1/(np.sqrt(2*np.pi*new_params['Deltat'])*var_sigma))
-                            -(ts[k]-ts[k-1]*(1+var_mu*new_params['Deltat']))**2/
-                             (2*var_sigma**2*new_params['Deltat']*ts[k-1]**2)
+                            -(ts.iloc[k]-ts.iloc[k-1]*(1+var_mu*new_params['Deltat']))**2/
+                             (2*var_sigma**2*new_params['Deltat']*ts.iloc[k-1]**2)
                            for k in range(1, ts.shape[0])])
             return nlL
 
@@ -119,18 +146,149 @@ class GBM(SimulacionMonteCarlo):
             print("ERROR: no se han ajustado los parámetros correctamente.")
 
         return
-                               
+
 
 class MertonJumpDiffusion(GBM):
     def __init__(self, param_dict=None):
         super().__init__(param_dict)
-        super().mostrar_parametros()
+        self.lbda = None
+        self.mu_J = None
+        self.sigma_J = None
+        if param_dict is not None:
+            self.informar_parametros(param_dict)
+        
+
+    def confirmar_parametros_validos(self, param_dict):
+        todo_bien = super().confirmar_parametros_validos(param_dict)
+        for var in ['lambda', 'sigma_J', 'mu_J']:
+            if var not in param_dict.keys():
+                print(f"ERROR: la variable {var} debe estar informada.")
+                todo_bien = False
+            if var != 'mu_J' and param_dict[var] <= 0:
+                print(f"ERROR: la variable {var} debe ser positiva.")
+                todo_bien = False
+                
+        return todo_bien
+
+    
+    def informar_parametros(self, param_dict):
+        if not self.confirmar_parametros_validos(param_dict):
+            return
+        super().informar_parametros(param_dict)
+        self.lbda = param_dict['lambda']
+        self.mu_J = param_dict['mu_J']
+        self.sigma_J = param_dict['sigma_J']
+        
+
+    def simular(self, S0=None, N=None, M=None):
+        S = super().simular(S0, N, M)
+        N = np.random.poisson(size=[self.N+1, self.M], lam=self.lbda*self.Deltat)
+        jumps = np.argwhere(N > 0)
+        
+        for j in range(self.M):
+            for i in range(self.N+1):
+                jump = N[i,j] * np.random.normal(loc=self.mu_J, scale=self.sigma_J)
+                if S[i,j] + jump <= 0:
+                    S[i:,j] = 0                    
+                    break
+                else:
+                    S[i:,j] += jump
+        return S
 
 
+    def ajustar_parametros(self, ts, M):
+        new_params = {'T': ts.shape[0], 'N': ts.shape[0], 'Deltat':1., 'S0':ts[0], 'M':M}
+        
+        def neg_log_likelihood(Theta):
+            k_MAX = 10
+            
+            mu, sigma = Theta[0], Theta[1]
+            lbda = Theta[2]
+            mu_J, sigma_J = Theta[3], Theta[4]
+
+            log_likelihood = 0.
+            for n in range(1, new_params['N']):
+                ksum = 0.
+                for k in range(k_MAX+1):
+                    loc = ts[n-1]*(1+mu*new_params['Deltat']) + k*mu_J
+                    scale = np.sqrt(ts[n-1]**2*new_params['Deltat']*sigma**2+k**2*sigma_J**2)
+                    density_norm = scipy.stats.norm.pdf(x=ts[n],loc=loc,scale=scale)
+                    mass_poisson = scipy.stats.poisson.pmf(k=k, mu=lbda)
+                    ksum += density_norm*mass_poisson
+                log_likelihood += np.log(ksum)
+            return -log_likelihood
+
+        def jac(Theta):
+            jac = np.zeros(5)
+            k_MAX = 5
+
+            mu, sigma = Theta[0], Theta[1]
+            lbda = Theta[2]
+            mu_J, sigma_J = Theta[3], Theta[4]
+
+            
+            for n in range(1, new_params['N']):
+                den = 0
+                for k in range(k_MAX+1):
+                    # calcular denominador
+                    loc = ts[n-1]*(1+mu*new_params['Deltat']) + k*mu_J
+                    scale = np.sqrt(ts[n-1]**2*new_params['Deltat']*sigma**2+k**2*sigma_J**2)
+                    density_norm = scipy.stats.norm.pdf(x=ts[n],loc=loc,scale=scale)
+                    mass_poisson = scipy.stats.poisson.pmf(k=k, mu=lbda)
+                    den += density_norm*mass_poisson
+                
+                d_mu, d_sigma, d_lbda, d_mu_J, d_sigma_J = 0,0,0,0,0
+                for k in range(k_MAX+1):
+                    # calcular derivadas de f
+                    M = ts[n-1]*(1+mu*new_params['Deltat']) + k*mu_J
+                    Sigma = (ts[n-1]*sigma)**2*new_params['Deltat'] + (k*sigma_J)**2
+                    
+                    d_mu += -np.sqrt(2)*new_params['Deltat']*ts[n-1]*(new_params['Deltat']*lbda)**k*(2*M - 2*ts[n])*np.exp(-new_params['Deltat']*lbda)*np.exp(-(-M + ts[n])**2/(2*Sigma**2))/(4*np.sqrt(np.pi)*Sigma**3*scipy.special.factorial(k))
+
+                    d_sigma += new_params['Deltat']*ts[n-1]**2*sigma*(-np.sqrt(2)*(new_params['Deltat']*lbda)**k*np.exp(-new_params['Deltat']*lbda)*np.exp(-(-M + ts[n])**2/(2*Sigma**2))/(2*np.sqrt(np.pi)*Sigma**2*scipy.special.factorial(k)) + np.sqrt(2)*(new_params['Deltat']*lbda)**k*(-M + ts[n])**2*np.exp(-new_params['Deltat']*lbda)*np.exp(-(-M + ts[n])**2/(2*Sigma**2))/(2*np.sqrt(np.pi)*Sigma**4*scipy.special.factorial(k)))/np.sqrt(new_params['Deltat']*ts[n-1]**2*sigma**2 + k**2*sigma_J**2)
+
+                    d_lbda += -np.sqrt(2)*new_params['Deltat']*(new_params['Deltat']*lbda)**k*np.exp(-new_params['Deltat']*lbda)*np.exp(-(-M + ts[n])**2/(2*Sigma**2))/(2*np.sqrt(np.pi)*Sigma*scipy.special.factorial(k)) + np.sqrt(2)*k*(new_params['Deltat']*lbda)**k*np.exp(-new_params['Deltat']*lbda)*np.exp(-(-M + ts[n])**2/(2*Sigma**2))/(2*np.sqrt(np.pi)*Sigma*lbda*scipy.special.factorial(k))
+
+                    d_mu_J += -np.sqrt(2)*k*(new_params['Deltat']*lbda)**k*(2*M - 2*ts[n])*np.exp(-new_params['Deltat']*lbda)*np.exp(-(-M + ts[n])**2/(2*Sigma**2))/(4*np.sqrt(np.pi)*Sigma**3*scipy.special.factorial(k))
 
 
+                    d_sigma_J += k**2*sigma_J*(-np.sqrt(2)*(new_params['Deltat']*lbda)**k*np.exp(-new_params['Deltat']*lbda)*np.exp(-(-M + ts[n])**2/(2*Sigma**2))/(2*np.sqrt(np.pi)*Sigma**2*scipy.special.factorial(k)) + np.sqrt(2)*(new_params['Deltat']*lbda)**k*(-M + ts[n])**2*np.exp(-new_params['Deltat']*lbda)*np.exp(-(-M + ts[n])**2/(2*Sigma**2))/(2*np.sqrt(np.pi)*Sigma**4*scipy.special.factorial(k)))/np.sqrt(new_params['Deltat']*ts[n-1]**2*sigma**2 + k**2*sigma_J**2)
+
+                    
+                jac += np.array([d_mu,d_sigma,d_lbda,d_mu_J,d_sigma_J])/den
+            return -jac
 
 
+                
+        
+        def callback(Theta):
+            print(f"mu:\t\t{Theta[0]}")
+            print(f"sigma:\t\t{Theta[1]}")
+            print(f"lambda:\t\t{Theta[2]}")
+            print(f"mu_J:\t\t{Theta[3]}")
+            print(f"sigma_J:\t{Theta[4]}")
+            print("")
+
+        opt_res = scipy.optimize.minimize(neg_log_likelihood, 0.5*np.ones(5), method='Newton-CG',
+                                          callback=callback, jac=jac,
+                                          options={'xatol': 1e-4, 'disp': False, 'verbose':1}, )
+
+        if opt_res.success:
+            new_params['mu'] = opt_res.x[0]
+            new_params['sigma'] = opt_res.x[1]
+            new_params['lambda'] = opt_res.x[2]
+            new_params['mu_J'] = opt_res.x[3]
+            new_params['sigma_J'] = opt_res.x[4]
+            self.informar_parametros(new_params)
+            print("Ajuste de parámetros realizado correctamente.")
+        else:
+            print("ERROR: no se han ajustado los parámetros correctamente.")
+
+            
+                    
+        
+
+        
 
 
 
--- datos.py
+++ datos.py
@@ -1,6 +1,7 @@
 import numpy as np
 import pandas as pd
 import matplotlib.pyplot as plt
+import datetime
 
 import requests
 
@@ -18,5 +19,20 @@ def download_stock_data(ticker):
     
     data_json = requests.get(URL)
     data = pd.DataFrame(data_json.json())
+    data.index = pd.to_datetime(data['date'])
+    data.drop(['symbol', 'date'], axis=1, inplace=True) 
+    data.sort_index(inplace=True)
 
     return data
+
+
+def next_k_bdays(k, start, delta=datetime.timedelta(days=1)):
+    bdays = pd.bdate_range(start=start, end=start+k*delta)
+    extended_k = k
+    while len(bdays) < k+1:
+        bdays = pd.bdate_range(start=start, end=start+extended_k*delta)
+        extended_k += 1
+
+    return bdays
+    
+    
