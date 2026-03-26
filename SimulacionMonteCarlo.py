7edefebb616e13b5e693ca5e631c318a59d67fa9
Jordi Pomada Foix <jordi.pomada@gmail.com>
2026-03-26 17:14:03 +0100

    Algoritmo EM.

--- SimulacionMonteCarlo.py
+++ SimulacionMonteCarlo.py
@@ -178,7 +178,10 @@ class MertonJumpDiffusion(GBM):
         self.lbda = param_dict['lambda']
         self.mu_J = param_dict['mu_J']
         self.sigma_J = param_dict['sigma_J']
-        
+
+    def mostrar_parametros(self):
+        super().mostrar_parametros()
+        pass
 
     def simular(self, S0=None, N=None, M=None):
         S = super().simular(S0, N, M)
@@ -211,7 +214,7 @@ class MertonJumpDiffusion(GBM):
                 ksum = 0.
                 for k in range(k_MAX+1):
                     loc = ts[n-1]*(1+mu*new_params['Deltat']) + k*mu_J
-                    scale = np.sqrt(ts[n-1]**2*new_params['Deltat']*sigma**2+k**2*sigma_J**2)
+                    scale = np.sqrt(ts[n-1]**2*new_params['Deltat']*sigma**2+k*sigma_J**2)
                     density_norm = scipy.stats.norm.pdf(x=ts[n],loc=loc,scale=scale)
                     mass_poisson = scipy.stats.poisson.pmf(k=k, mu=lbda)
                     ksum += density_norm*mass_poisson
@@ -220,7 +223,7 @@ class MertonJumpDiffusion(GBM):
 
         def jac(Theta):
             jac = np.zeros(5)
-            k_MAX = 5
+            k_MAX = 20
 
             mu, sigma = Theta[0], Theta[1]
             lbda = Theta[2]
@@ -232,7 +235,7 @@ class MertonJumpDiffusion(GBM):
                 for k in range(k_MAX+1):
                     # calcular denominador
                     loc = ts[n-1]*(1+mu*new_params['Deltat']) + k*mu_J
-                    scale = np.sqrt(ts[n-1]**2*new_params['Deltat']*sigma**2+k**2*sigma_J**2)
+                    scale = np.sqrt(ts[n-1]**2*new_params['Deltat']*sigma**2+k*sigma_J**2)
                     density_norm = scipy.stats.norm.pdf(x=ts[n],loc=loc,scale=scale)
                     mass_poisson = scipy.stats.poisson.pmf(k=k, mu=lbda)
                     den += density_norm*mass_poisson
@@ -241,46 +244,118 @@ class MertonJumpDiffusion(GBM):
                 for k in range(k_MAX+1):
                     # calcular derivadas de f
                     M = ts[n-1]*(1+mu*new_params['Deltat']) + k*mu_J
-                    Sigma = (ts[n-1]*sigma)**2*new_params['Deltat'] + (k*sigma_J)**2
+                    Sigma = np.sqrt((ts[n-1]*sigma)**2*new_params['Deltat'] + (k*sigma_J)**2)
                     
                     d_mu += -np.sqrt(2)*new_params['Deltat']*ts[n-1]*(new_params['Deltat']*lbda)**k*(2*M - 2*ts[n])*np.exp(-new_params['Deltat']*lbda)*np.exp(-(-M + ts[n])**2/(2*Sigma**2))/(4*np.sqrt(np.pi)*Sigma**3*scipy.special.factorial(k))
 
-                    d_sigma += new_params['Deltat']*ts[n-1]**2*sigma*(-np.sqrt(2)*(new_params['Deltat']*lbda)**k*np.exp(-new_params['Deltat']*lbda)*np.exp(-(-M + ts[n])**2/(2*Sigma**2))/(2*np.sqrt(np.pi)*Sigma**2*scipy.special.factorial(k)) + np.sqrt(2)*(new_params['Deltat']*lbda)**k*(-M + ts[n])**2*np.exp(-new_params['Deltat']*lbda)*np.exp(-(-M + ts[n])**2/(2*Sigma**2))/(2*np.sqrt(np.pi)*Sigma**4*scipy.special.factorial(k)))/np.sqrt(new_params['Deltat']*ts[n-1]**2*sigma**2 + k**2*sigma_J**2)
+
+                    d_sigma += new_params['Deltat']*ts[n-1]**2*sigma*(-np.sqrt(2)*(new_params['Deltat']*lbda)**k*np.exp(-new_params['Deltat']*lbda)*np.exp(-(-M + ts[n])**2/(2*Sigma**2))/(2*np.sqrt(np.pi)*Sigma**2*scipy.special.factorial(k)) + np.sqrt(2)*(new_params['Deltat']*lbda)**k*(-M + ts[n])**2*np.exp(-new_params['Deltat']*lbda)*np.exp(-(-M + ts[n])**2/(2*Sigma**2))/(2*np.sqrt(np.pi)*Sigma**4*scipy.special.factorial(k)))/np.sqrt(new_params['Deltat']*ts[n-1]**2*sigma**2 + k*sigma_J**2)
 
                     d_lbda += -np.sqrt(2)*new_params['Deltat']*(new_params['Deltat']*lbda)**k*np.exp(-new_params['Deltat']*lbda)*np.exp(-(-M + ts[n])**2/(2*Sigma**2))/(2*np.sqrt(np.pi)*Sigma*scipy.special.factorial(k)) + np.sqrt(2)*k*(new_params['Deltat']*lbda)**k*np.exp(-new_params['Deltat']*lbda)*np.exp(-(-M + ts[n])**2/(2*Sigma**2))/(2*np.sqrt(np.pi)*Sigma*lbda*scipy.special.factorial(k))
 
                     d_mu_J += -np.sqrt(2)*k*(new_params['Deltat']*lbda)**k*(2*M - 2*ts[n])*np.exp(-new_params['Deltat']*lbda)*np.exp(-(-M + ts[n])**2/(2*Sigma**2))/(4*np.sqrt(np.pi)*Sigma**3*scipy.special.factorial(k))
 
 
-                    d_sigma_J += k**2*sigma_J*(-np.sqrt(2)*(new_params['Deltat']*lbda)**k*np.exp(-new_params['Deltat']*lbda)*np.exp(-(-M + ts[n])**2/(2*Sigma**2))/(2*np.sqrt(np.pi)*Sigma**2*scipy.special.factorial(k)) + np.sqrt(2)*(new_params['Deltat']*lbda)**k*(-M + ts[n])**2*np.exp(-new_params['Deltat']*lbda)*np.exp(-(-M + ts[n])**2/(2*Sigma**2))/(2*np.sqrt(np.pi)*Sigma**4*scipy.special.factorial(k)))/np.sqrt(new_params['Deltat']*ts[n-1]**2*sigma**2 + k**2*sigma_J**2)
+                    d_sigma_J += k*sigma_J*(-np.sqrt(2)*(new_params['Deltat']*lbda)**k*np.exp(-new_params['Deltat']*lbda)*np.exp(-(-M + ts[n])**2/(2*Sigma**2))/(2*np.sqrt(np.pi)*Sigma**2*scipy.special.factorial(k)) + np.sqrt(2)*(new_params['Deltat']*lbda)**k*(-M + ts[n])**2*np.exp(-new_params['Deltat']*lbda)*np.exp(-(-M + ts[n])**2/(2*Sigma**2))/(2*np.sqrt(np.pi)*Sigma**4*scipy.special.factorial(k)))/np.sqrt(new_params['Deltat']*ts[n-1]**2*sigma**2 + k*sigma_J**2)
 
                     
                 jac += np.array([d_mu,d_sigma,d_lbda,d_mu_J,d_sigma_J])/den
             return -jac
 
-
+        def EM():
+            Theta_0 = np.ones(5)
+            Theta_l = np.ones(5)
+            
+            def E(Theta):
+                k_MAX = 1
                 
-        
-        def callback(Theta):
-            print(f"mu:\t\t{Theta[0]}")
-            print(f"sigma:\t\t{Theta[1]}")
-            print(f"lambda:\t\t{Theta[2]}")
-            print(f"mu_J:\t\t{Theta[3]}")
-            print(f"sigma_J:\t{Theta[4]}")
-            print("")
+                mu, sigma = Theta[0], Theta[1]
+                lbda = Theta[2]
+                mu_J, sigma_J = Theta[3], Theta[4]
 
-        opt_res = scipy.optimize.minimize(neg_log_likelihood, 0.5*np.ones(5), method='Newton-CG',
-                                          callback=callback, jac=jac,
-                                          options={'xatol': 1e-4, 'disp': False, 'verbose':1}, )
+                mu_l, sigma_l = Theta_l[0], Theta_l[1]
+                lbda_l = Theta_l[2]
+                mu_J_l, sigma_J_l = Theta_l[3], Theta_l[4]
+
+                Q = 0
+                for n in range(1, new_params['N']):
+                    ksum = 0
+                    for k in range(0, k_MAX+1):
+                        
+                        den = 0
+                        # usamos parámetros de paso l para calcular q (parametros _l)
+                        for kp in range(0, k_MAX+1):
+                            loc = ts[n-1]*(1+mu_l*new_params['Deltat']) + kp*mu_J_l
+                            scale = np.sqrt(ts[n-1]**2*new_params['Deltat']*sigma_l**2+kp*sigma_J_l**2)
+                            density_norm = scipy.stats.norm.pdf(x=ts[n],loc=loc,scale=scale)
+                            mass_poisson = scipy.stats.poisson.pmf(k=kp, mu=lbda)
+                            den += density_norm*mass_poisson
+                            
+                        loc = ts[n-1]*(1+mu_l*new_params['Deltat']) + k*mu_J_l
+                        scale = np.sqrt(ts[n-1]**2*new_params['Deltat']*sigma_l**2+k*sigma_J_l**2)
+                        density_norm = scipy.stats.norm.pdf(x=ts[n],loc=loc,scale=scale)
+                        mass_poisson = scipy.stats.poisson.pmf(k=k, mu=lbda)
+                        q = density_norm*mass_poisson/den
+
+                        # calculamos el valor de log(...)
+                        loc = ts[n-1]*(1+mu*new_params['Deltat']) + k*mu_J
+                        scale = np.sqrt(ts[n-1]**2*new_params['Deltat']*sigma**2+k*sigma_J**2)
+                        density_norm = scipy.stats.norm.pdf(x=ts[n],loc=loc,scale=scale)
+                        mass_poisson = scipy.stats.poisson.pmf(k=k, mu=lbda)
+                        argument_log = density_norm*mass_poisson
+
+                        ksum += q*np.log(argument_log)
+    
+                    Q += ksum
+                return Q
+
+            def M(Theta_0):
+                def callback(Theta):
+                    print(f"mu:\t\t{Theta[0]}")
+                    print(f"sigma:\t\t{Theta[1]}")
+                    print(f"lambda:\t\t{Theta[2]}")
+                    print(f"mu_J:\t\t{Theta[3]}")
+                    print(f"sigma_J:\t{Theta[4]}")
+                    print("")
+
+                bounds = [(-np.inf, np.inf), (0, np.inf), (0, np.inf), (-np.inf, np.inf), (0, np.inf)]
+
+                print("Primera optimización...:")
+                opt_res = scipy.optimize.minimize(lambda x: -E(x), Theta_0, method='L-BFGS-B',bounds=bounds,
+                                                  options={'disp':True},callback=callback)
+                if not opt_res.success:
+                    print("ERROR: EM: no se ha podido optimizar en el paso M.")
+                    return
+                Theta_l = Theta_0
+                Theta_new = opt_res.x
+                print(Theta_new)
+                while np.sum((Theta_new-Theta_l)**2)>0.1:
+                    Theta_l = Theta_new
+                    opt_res = scipy.optimize.minimize(lambda x: -E(x), Theta_l, method='L-BFGS-B',bounds=bounds,
+                                                  options={'disp':True},callback=callback)
+                    Theta_new = opt_res.x
+                return Theta_new
+
+            return M(Theta_0);
+
+        return EM()
+        
+        
+
+        bounds = [(-np.inf, np.inf), (0, np.inf), (0, np.inf), (-np.inf, np.inf), (0, np.inf)]
+        
+        opt_res = scipy.optimize.minimize(neg_log_likelihood, 5*np.ones(5), method='L-BFGS-B',
+                                          callback=callback, jac=jac,bounds=bounds,
+                                          options={'disp': True}, )
 
         if opt_res.success:
+            print("Ajuste de parámetros realizado correctamente.")
             new_params['mu'] = opt_res.x[0]
             new_params['sigma'] = opt_res.x[1]
             new_params['lambda'] = opt_res.x[2]
             new_params['mu_J'] = opt_res.x[3]
             new_params['sigma_J'] = opt_res.x[4]
             self.informar_parametros(new_params)
-            print("Ajuste de parámetros realizado correctamente.")
         else:
             print("ERROR: no se han ajustado los parámetros correctamente.")
 
