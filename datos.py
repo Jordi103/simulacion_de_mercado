daa4f85cee94cc7a67e044b417d4bea166da7dcf
Jordi Pomada Foix <jordi.pomada@gmail.com>
2026-04-05 16:47:40 +0200

    Generación de pasos intermedios por distintos métodos.

--- datos.py
+++ datos.py
@@ -1,7 +1,9 @@
 import numpy as np
 import pandas as pd
 import matplotlib.pyplot as plt
+import matplotlib.dates as mdates
 import datetime
+from datetime import timedelta
 
 import requests
 
@@ -26,7 +28,7 @@ def download_stock_data(ticker):
     return data
 
 
-def next_k_bdays(k, start, delta=datetime.timedelta(days=1)):
+def next_k_bdates(k, start, delta=datetime.timedelta(days=1)):
     is_bday = lambda date: True if len(pd.bdate_range(date, date)) == 1 else False
     if not is_bday(start):
         print("WARNING: La fecha de inicio no es laborable.")
@@ -66,17 +68,22 @@ class OHLC(pd.DataFrame):
         ax.bar(pricesdown.index,pricesdown.close-pricesdown.open,width,bottom=pricesdown.open,color='r')
         ax.bar(pricesdown.index,pricesdown.high-pricesdown.open,width2,bottom=pricesdown.open,color='gray')
         ax.bar(pricesdown.index,pricesdown.low-pricesdown.close,width2, bottom=pricesdown.close,color='gray')
-        
+
         ax.grid()
 
-    def desde_serie(self, valores, fechas):
-        # asumimos granularidad de las fechas mayor que diaria.
+        ax.set_xticks(self.index)
+        locator = mdates.AutoDateLocator()
+        ax.xaxis.set_major_locator(locator)
+        #ax.xaxis.set_major_formatter(mdates.ConciseDateFormatter(locator))
+
+
         
-        if len(valores) != len(fechas):
-            print("ERROR: valores y fechas no tienen la misma longitud.")
-            return
-        df = pd.DataFrame({'fechas': fechas, 'valores': valores})
-        df['dia'] = df['fechas'].dt.day
+
+    def desde_simulacion(self, serie):
+        # asumimos granularidad de las fechas mayor que diaria.
+
+        df = serie.reset_index()
+        df['dia'] = df['fechas'].dt.floor("D")
         
         df_final = df.drop('fechas',axis=1).groupby('dia')
         agg_open = pd.NamedAgg('valores', 'first')
@@ -84,7 +91,63 @@ class OHLC(pd.DataFrame):
         agg_high = pd.NamedAgg('valores', 'max')
         agg_low = pd.NamedAgg('valores', 'min')
         df_final = df_final.agg(open=agg_open, close=agg_close, high=agg_high, low=agg_low)
-    
+
         return OHLC(df_final)
+
+    
+    def desde_volatilidad_relativa(self, serie_con_fechas):
+
+        df = pd.DataFrame()
+        # std = serie_con_fechas.std()/np.sqrt(len(serie_con_fechas))
+        std = serie_con_fechas.rolling(5).std()/np.sqrt(5)
         
+        df['close'] = serie_con_fechas
+        df['open'] = serie_con_fechas.shift(-1)
+        df['high'] = np.maximum(df['close'], df['open']) + np.abs(np.random.normal(scale=std))
+        df['low'] = np.minimum(df['close'], df['open']) - np.abs(np.random.normal(scale=std))
+        
+        return OHLC(df.dropna())
+
+
+    def desde_interpolacion_puentes_brownianos(self, serie_con_fechas, n_subpasos=20):
+        
+        df = pd.DataFrame()
+        total_dias = len(pd.bdate_range(start=serie_con_fechas.index[0],
+                                        end=serie_con_fechas.index[-1]))
+    
+        df.index = next_k_bdates(total_dias*n_subpasos, 
+                                 start=serie_con_fechas.index[0],
+                                 delta=timedelta(minutes=24*60/n_subpasos))
+
+        Deltat_subpasos = 1./n_subpasos
+        for i in range(len(serie_con_fechas)-1):
+            a = serie_con_fechas.iloc[i]
+            b = serie_con_fechas.iloc[i+1]
+            T_subpasos = (serie_con_fechas.index[i+1]-serie_con_fechas.index[i]).total_seconds()/(60.*60*24)
+            # Deltat_subpasos = T_subpasos/n_subpasos
+            n_sp_total = int(T_subpasos/Deltat_subpasos)
+            B = np.random.normal(size=n_sp_total-1,
+                                 scale=serie_con_fechas.std()*np.sqrt(Deltat_subpasos))\
+                .cumsum(axis=0)
+            B = np.array([0, *B])
+            bridge_00 = B - np.arange(n_sp_total)*Deltat_subpasos/T_subpasos * B[-1]
+            bridge_ab = a + np.arange(n_sp_total)*Deltat_subpasos/T_subpasos*(b-a) + bridge_00
+            fechas_ab = next_k_bdates(k=n_sp_total,
+                                      start=serie_con_fechas.index[i],
+                                      delta=timedelta(days=Deltat_subpasos))
+                
+            df.loc[fechas_ab, 'valores'] = bridge_ab
+
+        df.dropna(inplace=True)
+
+        # serie_con_fechas.reset_index(drop=True).plot()
+        # df.reset_index(drop=True).plot()
+        return self.desde_simulacion(df['valores'], df.index)
+
+
+
+
+
+
+    
                 
